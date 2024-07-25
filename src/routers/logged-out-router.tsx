8c8f4b09e9275207faa9fca33d2115b2708841a5
leejeawon92 <jeawon33333@gmail.com>
2024-07-25 22:39:09 +0900

    Implementing a Login Window Using React Hook Form

---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const LoggedOutRouter = () => {
+  const { register, watch, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log("cant create account");
+  };
+  console.log(errors);
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "This is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            {...register("email", { required: true })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
+    </div>
+  );
+};
