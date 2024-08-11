378377e964e070ed118483ac49daf5e5ab88d95d
leejeawon92 <jeawon33333@gmail.com>
2024-08-11 22:04:21 +0900

    restaurants query

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { verifyEmail, verifyEmailVariables} from "../../__generated__/verifyEmail";
 import { useMe } from '../../hooks/useMe';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 
 const VERIFY_EMAIL_MUTATION = gql`
@@ -58,6 +59,9 @@ export const ConfirmEmail = () => {
 
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Verify Email | Nuber Eats</title>
+      </Helmet>
       <h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
       <h4 className="text-gray-700 text-sm">
         Please wait, don't close this page...
--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
 import { useMe } from "../../hooks/useMe";
 import { editProfile, editProfileVariables} from "../../__generated__/editProfile";
+import { Helmet } from "react-helmet";
 
 const EDIT_PROFILE_MUTATION = gql`
   mutation editProfile($input: EditProfileInput!) {
@@ -72,6 +73,9 @@ export const EditProfile = () => {
   };
   return (
     <div className="mt-52 flex flex-col justify-center items-center">
+      <Helmet>
+        <title>Edit Profile | Nuber Eats</title>
+      </Helmet>
       <h4 className="font-semibold text-2xl mb-3">Edit Profile</h4>
       <form
         onSubmit={handleSubmit(onSubmit)}
