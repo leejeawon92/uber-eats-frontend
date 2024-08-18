b22db9d0b26f2e5a5d63654556d168cb83f88a14
leejeawon92 <jeawon33333@gmail.com>
2024-08-19 08:55:49 +0900

    test setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo";
 import { HelmetProvider } from 'react-helmet-async';
+import { App } from "./components/app";
 
 ReactDOM.render(
   <React.StrictMode>
